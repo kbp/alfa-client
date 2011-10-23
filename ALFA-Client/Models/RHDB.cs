27864deb07138c3e1d032630a57e6fe841187737
silence <blg.silence.com@gmail.com>
2011-10-23 16:15:39 +1000

    rhdb user

--- ALFA-Client/Models/RHDB.cs
+++ ALFA-Client/Models/RHDB.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
@@ -14,23 +11,53 @@ namespace ALFA_Client.Models
         {
         }
 
-        public void getKeyParams(int roomNumber)
+        public RHDBUser GetKeyParams(int roomNumber)
         {
-            SqlConnection conn = new SqlConnection();
-            conn.ConnectionString = System.Configuration.ConfigurationManager.AppSettings["RHDB"];
-            SqlCommand myCommand = conn.CreateCommand();
-            myCommand.CommandType = CommandType.StoredProcedure;
-            myCommand.CommandText = "Alfa_Locks2";
-            myCommand.Parameters.Add("Numroom", SqlDbType.VarChar).Value = roomNumber.ToString();
-            conn.Open();
+            RHDBUser user = null;
 
-            SqlDataReader dataReader = myCommand.ExecuteReader();
-            while (dataReader.Read())
+            SqlConnection conn = new SqlConnection();
+
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["RHDB"];
+
+            if (settings != null)
             {
-//                listBox1.Items.Add(dataReader.GetString(1) + "" + dataReader.GetString(2));
+                conn.ConnectionString = settings.ConnectionString;
+
+                SqlCommand myCommand = conn.CreateCommand();
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.CommandText = "Alfa_Locks2";
+                myCommand.Parameters.Add("Numroom", SqlDbType.VarChar).Value = roomNumber.ToString();
+                conn.Open();
+
+                SqlDataReader dataReader = myCommand.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    user = new RHDBUser();
+                    user.RoomNumber = int.Parse(dataReader["nuroom"].ToString());
+                    user.FIO = dataReader["fio"].ToString();
+                    user.Arriv = DateTime.Parse(dataReader["Arriv"].ToString());
+                    user.Depar = DateTime.Parse(dataReader["Depar"].ToString());
+                    user.Sex = byte.Parse(dataReader["sex"].ToString());
+                    user.Guestidn = long.Parse(dataReader["Guestidn"].ToString());
+
+
+                }
+                dataReader.Close();
+                conn.Close();
+
+
             }
-            dataReader.Close();
-            conn.Close();
+            return user;
         }
+    }
+
+    public class RHDBUser
+    {
+        public int RoomNumber { set; get; }
+        public string FIO { set; get; }
+        public DateTime Arriv { set; get; }
+        public DateTime Depar { set; get; }
+        public byte Sex { set; get; }
+        public long Guestidn { set; get; }
     }
 }
